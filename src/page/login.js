cdc663a48f6c1834e1554a251807bb0bd9bd7f8b
gholami71 <careless.whisper51@yahoo.com>
2023-07-03 12:00:43 +0430

    rbmanagment

---
+++ src/page/login.js
@@ -0,0 +1,14 @@
+const Login = () => {
+    return(
+        <div className="login">
+            
+            <input placeholder="نام کاربری"></input>
+            
+            <input placeholder="رمز عبور"></input>
+            <button>ورود</button>
+
+        </div>
+    )
+}
+
+export default Login
