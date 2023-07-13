2964b8f1274d984af1d74eb761fc65c5a6b94216
moeendehqan <moeen.dehqan@gmail.com>
2023-07-13 20:36:42 +0330

    users compllet

--- src/config/OnRun.js
+++ src/config/OnRun.js
@@ -1,2 +1,2 @@
-const OnRun = 'http://192.168.20.126:5000'
+const OnRun = 'http://127.0.0.1:5000'
 export default OnRun
--- src/page/sub/Users.js
+++ src/page/sub/Users.js
@@ -1,10 +1,46 @@
+import axios from "axios"
+import OnRun from "../../config/OnRun"
+import { useEffect, useState } from "react"
+import { getCookie } from "../../function/cookie"
+
+import {TabulatorFull as Tabulator} from 'tabulator-tables';
+
+const Users = () => {
+    const key = getCookie('uph')
+    const [df, setDf] = useState([])
 
 
+    var table = new Tabulator("#table", {
+        height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+        data:df, //assign data to table
+        layout:"fitColumns", //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"شماره", field:"phone",},
+            {title:"ثبت نام", field:"dateregister",},
+            {title:"اعتبار", field:"datecredit"},
+            {title:"نوع حساب", field:"label", },
+            {title:"اخرین بازدید", field:"lastlogin", },
+        ],
+   });
 
-const Users = () =>{
-    return(
+
+    const getDataAllUsers = () => {
+        axios.post(OnRun + '/management/getdataallusers', { key: key })
+        .then(response=>{
+            console.log(response.data)
+            if (response.data.reply) {
+                setDf(response.data.df)
+            }else{
+                alert(response.data.msg)
+            }
+        })
+    }
+
+    useEffect(getDataAllUsers,[])
+
+    return (
         <div className="sub">
-            Users
+            <div id="table"></div>
 
         </div>
     )
