1e9e4406e4d8e56be97ca9084c460b052c275828
moeendehqan <moeen.dehqan@gmail.com>
2023-07-15 00:24:20 +0330

    ///

--- src/page/sub/Ticket.js
+++ src/page/sub/Ticket.js
@@ -1,7 +1,27 @@
+import axios from "axios"
+import OnRun from "../../config/OnRun"
+import { useEffect, useState } from "react"
+import { getCookie } from "../../function/cookie"
 
 
 
 const Ticket = () =>{
+    const key = getCookie('uph')
+    const [df, setDf] = useState([])
+
+
+    const getTikets = () =>{
+        axios.post(OnRun+'/managment/gettickets',{key:key})
+        .then(response=>{
+            if (response.data.reply) {
+                setDf(response.data.df)
+            }else{
+                alert(response.data.msg)
+            }
+        })
+    }
+
+    useEffect(getTikets,[])
     return(
         <div className="sub">
             Ticket
