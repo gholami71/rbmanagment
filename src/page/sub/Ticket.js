f3701e39e4100e7b6cbac0384d1970834b464932
moeendehqan <moeen.dehqan@gmail.com>
2023-07-15 21:58:55 +0330

    // تموم شد خدا رو شکر

--- src/page/sub/Ticket.js
+++ src/page/sub/Ticket.js
@@ -2,13 +2,36 @@ import axios from "axios"
 import OnRun from "../../config/OnRun"
 import { useEffect, useState } from "react"
 import { getCookie } from "../../function/cookie"
+import {TabulatorFull as Tabulator} from 'tabulator-tables';
 
 
 
 const Ticket = () =>{
     const key = getCookie('uph')
     const [df, setDf] = useState([])
+    const [ansewr, setAnsewr] = useState(null)
 
+
+
+    var table = new Tabulator("#table", {
+        data:df,
+        layout:"fitColumns", //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"شماره", field:"user"},
+            {title:"عنوان تیکت", field:"title",},
+            {title:"پیام", field:"content"},
+            {title:"تاریخ", field:"dateJalali", },
+            {title:"ساعت", field:"time", },
+            {title:"حذف توسط کاربر", field:"del", },
+            {title:"پاسخ", field:"reply",
+                cellClick:(e, cell)=>{
+                    let row = cell.getRow();
+                    let data = row.getData();
+                    setAnsewr(data)
+                },
+            },
+        ],
+   });
 
     const getTikets = () =>{
         axios.post(OnRun+'/managment/gettickets',{key:key})
@@ -21,11 +44,33 @@ const Ticket = () =>{
         })
     }
 
+    const setReplyTicket = () =>{
+        if (ansewr.reply=='') {alert('پاسخ صحیح نیست')
+        }else{
+            axios.post(OnRun+'/managment/setreplyticket',{ansewr:ansewr,key:key})
+            .then(response=>{
+                if (response.data.reply) {
+                    alert('ثبت شد')
+                }else{
+                    alert(response.data.msg)
+                }
+            })
+        }
+    }
+
     useEffect(getTikets,[])
     return(
         <div className="sub">
-            Ticket
-
+            <div id="table"></div>
+            {
+                ansewr==null?null:
+                <div className="popup">
+                    <p>پاسخ به: {ansewr.user}</p>
+                    <textarea value={ansewr.reply} onChange={(e)=>setAnsewr({...ansewr,reply:e.target.value})}></textarea>
+                    <button onClick={setReplyTicket}>ثبت</button>
+                    <button onClick={()=>setAnsewr(null)}>لغو</button>
+                </div>
+            }
         </div>
     )
 }
