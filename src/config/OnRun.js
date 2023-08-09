33aa31b60e5039e273e97c99b1d36fc6df353568
moeen dehqan <moeen.dehqan@gmail.com>
2023-08-09 14:30:41 +0330

    //

--- src/config/OnRun.js
+++ src/config/OnRun.js
@@ -1,2 +1,2 @@
-const OnRun = 'http://api.roundtrade.ir' /*'http://127.0.0.1:5000'*/ 
+const OnRun = 'http://127.0.0.1:5000' /*'http://127.0.0.1:5000' https://api.roundtrade.ir*/ 
 export default OnRun
